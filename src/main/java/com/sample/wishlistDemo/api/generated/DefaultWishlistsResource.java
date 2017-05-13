120695ef8f9ae510fcb8528e64b87bf36758f58a
zhaozhiming <zhaozhiming003@gmail.com>
2017-05-13 13:48:36 +0800

    feat: first commit

---
+++ src/main/java/com/sample/wishlistDemo/api/generated/DefaultWishlistsResource.java
@@ -0,0 +1,86 @@
+
+package com.sample.wishlistDemo.api.generated;
+
+import javax.inject.Singleton;
+import javax.ws.rs.core.Response;
+
+import org.springframework.stereotype.Component;
+
+import com.sample.wishlistDemo.api.generated.WishlistItem;
+import com.sample.wishlistDemo.api.generated.YaasAwareParameters;
+
+/**
+* Resource class containing the custom logic. Please put your logic here!
+*/
+@Component("apiWishlistsResource")
+@Singleton
+public class DefaultWishlistsResource implements com.sample.wishlistDemo.api.generated.WishlistsResource
+{
+	@javax.ws.rs.core.Context
+	private javax.ws.rs.core.UriInfo uriInfo;
+
+	/* GET / */
+	@Override
+	public Response get(final YaasAwareParameters yaasAware)
+	{
+		// place some logic here
+		return Response.ok()
+			.entity(new java.util.ArrayList<Wishlist>()).build();
+	}
+
+	/* POST / */
+	@Override
+	public Response post(final YaasAwareParameters yaasAware, final Wishlist wishlist)
+	{
+		// place some logic here
+		return Response.created(uriInfo.getAbsolutePath())
+			.build();
+	}
+
+	/* GET /{wishlistId} */
+	@Override
+	public Response getByWishlistId(final YaasAwareParameters yaasAware, final java.lang.String wishlistId)
+	{
+		// place some logic here
+		return Response.ok()
+			.entity(new Wishlist()).build();
+	}
+
+	/* PUT /{wishlistId} */
+	@Override
+	public Response putByWishlistId(final YaasAwareParameters yaasAware, final java.lang.String wishlistId, final Wishlist wishlist)
+	{
+		// place some logic here
+		return Response.ok()
+			.build();
+	}
+
+	/* DELETE /{wishlistId} */
+	@Override
+	public Response deleteByWishlistId(final YaasAwareParameters yaasAware, final java.lang.String wishlistId)
+	{
+		// place some logic here
+		return Response.noContent()
+			.build();
+	}
+
+	@Override
+	public
+	Response getByWishlistIdWishlistItems(
+			final YaasAwareParameters yaasAware,  final java.lang.String wishlistId)
+	{
+		// place some logic here
+		return Response.ok()
+				.entity(new java.util.ArrayList<WishlistItem>()).build();
+	}
+
+	@Override
+	public
+	Response postByWishlistIdWishlistItems(final YaasAwareParameters yaasAware,
+			final java.lang.String wishlistId, final WishlistItem wishlistItem){
+		// place some logic here
+		return Response.noContent()
+					.build();
+	}
+
+}
